36a359b8af4411289196447899b17981626c795e
BJCheng <BJCheng@users.noreply.github.com>
2019-06-22 12:20:09 -0400

    finish web module setup

--- web/src/main/java/ben/learn/WebMain.java
+++ web/src/main/java/ben/learn/WebMain.java
@@ -1,4 +1,12 @@
 package ben.learn;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class WebMain {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebMain.class, args);
+	}
 }
